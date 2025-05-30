668a86eedff16a8d1400dca92937f99c6755a716
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2025-05-30 11:04:22 -0400

    Refactor

--- assignment-spotify/src/main/scala/spotify/dao/Artist.scala
+++
@@ -1,7 +0,0 @@
-package spotify.dao
-
-case class Artist(id: String, name: String, followers_count: Int)
-
-object Artist {
-    implicit val rw: upickle.default.ReadWriter[Artist] = upickle.default.macroRW
-}
--- assignment-spotify/src/main/scala/spotify/dao/PlaylistTracksPart.scala
+++
@@ -1,7 +0,0 @@
-package spotify.dao
-
-case class PlaylistTracksPart(playListId: String, offset: Int, limit: Int, total: Int, tracks: List[Track])
-
-object PlaylistTracksPart {
-    implicit val rw: upickle.default.ReadWriter[PlaylistTracksPart] = upickle.default.macroRW
-}
